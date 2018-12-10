f7060cd3cd252d2fc3725e1ff941e7e7e742e51d
samo451 <44391795+samo451@users.noreply.github.com>
2018-12-10 08:25:44 +0100

    Add files via upload
    Prvy commit

---
+++ du2/Du2.py
@@ -0,0 +1,18 @@
+import json
+from abc import ABC, abstractmethod
+
+
+class Body(ABC):
+
+
+def otvorenie_json (vstup):
+    with open
+
+
+def ulozenie_json:
+    with open...
+
+
+def delenie_bodov:
+    m = 1
+
