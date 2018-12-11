57c5ea0153b481f1ddccde07af03a1832a1764f5
Samo <Polopoklop741>
2018-12-11 22:26:54 +0100

    Cluster_ID a delenie funguju (snad)

--- du2/Testovanie.py
+++ du2/Testovanie.py
@@ -56,12 +56,12 @@ def pridanie_cluster_id(vstup, cluster_id):
     """Vstup jednej feature zo zoznamu, pridava cluster id podľa kľúča v dokumentácii"""
     zaznam = vstup['properties']
     try:
-        var = zaznam["cluster_id"]
+        var = zaznam['cluster_id']
     except KeyError:
         var = 0
     new_cluster_id = var*10 + cluster_id
-    zaznam.update({"cluster_id": new_cluster_id})
-    return zaznam
+    zaznam.update({'cluster_id': new_cluster_id})
+    return new_cluster_id
 
 q = calculator_bbox(gj)
 print(q)
@@ -75,6 +75,7 @@ def delenie_bodov(bbox, vstup):
     maxx = bbox[2]
     maxy = bbox[3]
     pocet_1, pocet_2, pocet_3, pocet_4 = 0, 0, 0, 0
+    cluster_id_1, cluster_id_2, cluster_id_3, cluster_id_4 = 0, 0, 0, 0
     x_mean = (maxx+minx)/2
     y_mean = (maxy+miny)/2
     print(x_mean, y_mean)
@@ -82,20 +83,69 @@ def delenie_bodov(bbox, vstup):
     for p in vstup['features']:
         if p['geometry']['coordinates'][0] < x_mean and p['geometry']['coordinates'][1] > y_mean:
             pocet_1 += 1
-            pridanie_cluster_id(p, 1)
+            cluster_id_1 = pridanie_cluster_id(p, 1)
         if p['geometry']['coordinates'][0] > x_mean and p['geometry']['coordinates'][1] > y_mean:
             pocet_2 += 1
-            pridanie_cluster_id(p, 2)
+            cluster_id_2 = pridanie_cluster_id(p, 2)
         if p['geometry']['coordinates'][0] < x_mean and p['geometry']['coordinates'][1] < y_mean:
             pocet_3 += 1
-            pridanie_cluster_id(p, 3)
+            cluster_id_3 = pridanie_cluster_id(p, 3)
         if p['geometry']['coordinates'][0] > x_mean and p['geometry']['coordinates'][1] < y_mean:
             pocet_4 += 1
-            pridanie_cluster_id(p, 4)
+            cluster_id_4 = pridanie_cluster_id(p, 4)
     print(pocet_1, pocet_2, pocet_4, pocet_3)
-    """if pocet_1 > 50:
+    if pocet_1 > 50:
         bbox_1 = [minx, y_mean, x_mean, maxy]
-        delenie_bodov(bbox_1, )"""
+        delenie_bodov_opakovane(cluster_id_1, bbox_1, vstup)
+    if pocet_2 > 50:
+        bbox_2 = [x_mean, y_mean, maxx, maxy]
+        delenie_bodov_opakovane(cluster_id_2, bbox_2, vstup)
+    if pocet_3 > 50:
+        bbox_3 = [minx, miny, x_mean, y_mean]
+        delenie_bodov_opakovane(cluster_id_3, bbox_3, vstup)
+    if pocet_4 > 50:
+        bbox_4 = [x_mean, miny, maxx, y_mean]
+        delenie_bodov_opakovane(cluster_id_4, bbox_4, vstup)
+
+
+
+def delenie_bodov_opakovane(cluster_id, bbox, vstup):
+    minx = bbox[0]
+    miny = bbox[1]
+    maxx = bbox[2]
+    maxy = bbox[3]
+    pocet_1, pocet_2, pocet_3, pocet_4 = 0, 0, 0, 0
+    x_mean = (maxx + minx) / 2
+    y_mean = (maxy + miny) / 2
+    print(x_mean, y_mean)
+
+    for p in vstup['features']:
+        if p['geometry']['coordinates'][0] < x_mean and p['geometry']['coordinates'][1] > y_mean and p['properties']['cluster_id'] == cluster_id:
+            pocet_1 += 1
+            cluster_id_1 = pridanie_cluster_id(p, 1)
+        if p['geometry']['coordinates'][0] > x_mean and p['geometry']['coordinates'][1] > y_mean and p['properties']['cluster_id'] == cluster_id:
+            pocet_2 += 1
+            cluster_id_2 = pridanie_cluster_id(p, 2)
+        if p['geometry']['coordinates'][0] < x_mean and p['geometry']['coordinates'][1] < y_mean and p['properties']['cluster_id'] == cluster_id:
+            pocet_3 += 1
+            cluster_id_3 = pridanie_cluster_id(p, 3)
+        if p['geometry']['coordinates'][0] > x_mean and p['geometry']['coordinates'][1] < y_mean and p['properties']['cluster_id'] == cluster_id:
+            pocet_4 += 1
+            cluster_id_4 = pridanie_cluster_id(p, 4)
+    print(pocet_1, pocet_2, pocet_4, pocet_3)
+    if pocet_1 > 50:
+        bbox_1 = [minx, y_mean, x_mean, maxy]
+        delenie_bodov_opakovane(cluster_id_1, bbox_1, vstup)
+    if pocet_2 > 50:
+        bbox_2 = [x_mean, y_mean, maxx, maxy]
+        delenie_bodov_opakovane(cluster_id_2, bbox_2, vstup)
+    if pocet_3 > 50:
+        bbox_3 = [minx, miny, x_mean, y_mean]
+        delenie_bodov_opakovane(cluster_id_3, bbox_3, vstup)
+    if pocet_4 > 50:
+        bbox_4 = [x_mean, miny, maxx, y_mean]
+        delenie_bodov_opakovane(cluster_id_4, bbox_4, vstup)
+
 
 delenie_bodov(calculator_bbox(gj), gj)
 #print(gj)
