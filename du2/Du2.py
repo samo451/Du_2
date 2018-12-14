cc1d54726a51c4ce5713a41ab1524b869d54302c
Samo <Polopoklop741>
2018-12-14 22:15:23 +0100

    Konsolidácia kódu: spojenie delení do jednej funkcie
    Nové hodnoty sa nepridávajú upadatom

--- du2/Du2.py
+++ du2/Du2.py
@@ -20,48 +20,7 @@ except UnicodeDecodeError:
     exit(3)
 
 
-def delenie_bodov(bbox, vstup):
-    """Delenie bodov pomocou mechanizmu quadtree. Iba pre inicializačný beh"""
-    minx = bbox[0]
-    miny = bbox[1]
-    maxx = bbox[2]
-    maxy = bbox[3]
-    pocet_1, pocet_2, pocet_3, pocet_4 = 0, 0, 0, 0
-    cluster_id_1, cluster_id_2, cluster_id_3, cluster_id_4 = 0, 0, 0, 0
-    x_mean = (maxx+minx)/2
-    y_mean = (maxy+miny)/2
-
-    for p in vstup['features']:
-        """Samotné roztriedenie na 4 oblasti a následné priradenie cluster_id atribútu"""
-        if p['geometry']['coordinates'][0] < x_mean and p['geometry']['coordinates'][1] > y_mean:
-            pocet_1 += 1
-            cluster_id_1 = pridanie_cluster_id(p, 1)
-        if p['geometry']['coordinates'][0] > x_mean and p['geometry']['coordinates'][1] > y_mean:
-            pocet_2 += 1
-            cluster_id_2 = pridanie_cluster_id(p, 2)
-        if p['geometry']['coordinates'][0] < x_mean and p['geometry']['coordinates'][1] < y_mean:
-            pocet_3 += 1
-            cluster_id_3 = pridanie_cluster_id(p, 3)
-        if p['geometry']['coordinates'][0] > x_mean and p['geometry']['coordinates'][1] < y_mean:
-            pocet_4 += 1
-            cluster_id_4 = pridanie_cluster_id(p, 4)
-    """Zisťovanie, či je počet bodov v jednom  clustri nad 50. Pokiaě áno, delenie bodov sa opakuje už opakovacou
-    deliacou funkciou"""
-    if pocet_1 > 50:
-        bbox_1 = [minx, y_mean, x_mean, maxy]
-        delenie_bodov_opakovane(cluster_id_1, bbox_1, vstup)
-    if pocet_2 > 50:
-        bbox_2 = [x_mean, y_mean, maxx, maxy]
-        delenie_bodov_opakovane(cluster_id_2, bbox_2, vstup)
-    if pocet_3 > 50:
-        bbox_3 = [minx, miny, x_mean, y_mean]
-        delenie_bodov_opakovane(cluster_id_3, bbox_3, vstup)
-    if pocet_4 > 50:
-        bbox_4 = [x_mean, miny, maxx, y_mean]
-        delenie_bodov_opakovane(cluster_id_4, bbox_4, vstup)
-
-
-def delenie_bodov_opakovane(cluster_id, bbox, vstup):
+def delenie_bodov(cluster_id, bbox, vstup):
     """Delenie bodov pomocou mechanizmu quadtree. Iba pre pokračujúci beh (musí existovať atribút cluster_id)"""
     minx = bbox[0]
     miny = bbox[1]
@@ -74,6 +33,10 @@ def delenie_bodov_opakovane(cluster_id, bbox, vstup):
 
     for p in vstup['features']:
         """Samotné roztriedenie na 4 oblasti a následné priradenie cluster_id atribútu"""
+        try:
+            test = p['properties']['cluster_id']
+        except:
+            p['properties']['cluster_id'] = 0
         if p['geometry']['coordinates'][0] < x_mean and p['geometry']['coordinates'][1] > y_mean and p['properties']['cluster_id'] == cluster_id:
             pocet_1 += 1
             cluster_id_1 = pridanie_cluster_id(p, 1)
@@ -109,11 +72,11 @@ def pridanie_cluster_id(vstup, cluster_id):
     except KeyError:
         var = 0
     new_cluster_id = var*10 + cluster_id
-    zaznam.update({'cluster_id': new_cluster_id})
+    zaznam['cluster_id'] = new_cluster_id
     return new_cluster_id
 
 
-def calculator_bbox(vstup):
+def calculate_bbox(vstup):
     """Zistí bbox a vracia ho ako zoznam minx, miny, maxx, maxy"""
     minx, miny = float("inf"), float("inf")
     maxx, maxy = float("-inf"), float("-inf")
@@ -130,7 +93,7 @@ def calculator_bbox(vstup):
     return bbox
 
 
-delenie_bodov(calculator_bbox(geojson), geojson)
+delenie_bodov(0, calculate_bbox(geojson), geojson)
 
 with open(filename_export, 'w') as geojson_file:
     json.dump(geojson, geojson_file)
